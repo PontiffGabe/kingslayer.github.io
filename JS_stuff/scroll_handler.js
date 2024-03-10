f2955523727bc3cceeffcd80ef215d7b71485455
PontiffGabe <129831757+PontiffGabe@users.noreply.github.com>
2024-03-10 16:08:44 +0100

    window drag scroll thing

--- JS_stuff/scroll_handler.js
+++ JS_stuff/scroll_handler.js
@@ -17,3 +17,32 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((elt) => observer.observe(elt));
+
+
+
+
+const wrapper = document.querySelector(".wrapper");
+header = wrapper.querySelector("header");
+
+function onDrag({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    wrapper.style.left = `${left + movementX}px` 
+    wrapper.style.top = `${top + movementY}px`
+}
+
+header.addEventListener("mousedown", () => {
+    header.addEventListener("mousemove", onDrag);
+});
+
+header.addEventListener("mouseup", () => {
+    header.removeEventListener("mousemove", onDrag);
+});
+
+header.addEventListener("mouseleave", () => {
+    header.removeEventListener("mousemove", onDrag);
+});
