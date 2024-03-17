ec366356b9dc56e9f69ebdd6a6739c039e272120
PontiffGabe <129831757+PontiffGabe@users.noreply.github.com>
2024-03-17 15:12:41 +0100

    dadazd

--- app.js
+++ app.js
@@ -94,49 +94,29 @@ function showPerson(person) {
       // get time components
       const hours = today.getHours();
       const minutes = today.getMinutes();
-      const seconds = today.getSeconds();
   
       //add '0' to hour, minute & second when they are less 10
       const hour = hours < 10 ? "0" + hours : hours;
       const minute = minutes < 10 ? "0" + minutes : minutes;
-      const second = seconds < 10 ? "0" + seconds : seconds;
-  
-      //make clock a 12-hour time clock
-      const hourTime = hour > 12 ? hour - 12 : hour;
   
       // if (hour === 0) {
       //   hour = 12;
       // }
-      //assigning 'am' or 'pm' to indicate time of the day
-      const ampm = hour < 12 ? "AM" : "PM";
   
       // get date components
       const month = today.getMonth();
       const year = today.getFullYear();
       const day = today.getDate();
-  
-      //declaring a list of all months in  a year
-      const monthList = [
-        "Janvier",
-        "Février",
-        "Mars",
-        "Avril",
-        "Mai",
-        "Juin",
-        "Juillet",
-        "Août",
-        "Septembre",
-        "Octobre",
-        "Novembre",
-        "Décembre"
-      ];
+
+      const fmonth = month < 10 ? "0" + month : month;
+      const fday = day < 10 ? "0" + day : day;
   
       //get current date and time
-      const date = monthList[month] + " " + day + ", " + year;
-      const time = hourTime + ":" + minute + ":" + second + ampm;
+      const date = fday + "/" + fmonth + "/" + year;
+      const time = hour + ":" + minute;
   
       //combine current date and time
-      const dateTime = date + " - " + time;
+      const dateTime = time + " - " + date;
   
       //print current date and time to the DOM
       document.getElementById("date-time").innerHTML = dateTime;
