338228d55377c46b25475e4119c947e59a630b27
PontiffGabe <129831757+PontiffGabe@users.noreply.github.com>
2024-03-19 22:42:16 +0100

    for jimmy

--- app.js
+++ app.js
@@ -49,8 +49,14 @@ closeDownloadButton.addEventListener("click", () => {
 // ----------------------------------
 
 // ------------- DRAGGABLE DIV --------------
-const about_us_wrapper = document.getElementById(".wrapper-about_us");
+const about_us_wrapper = document.getElementById("wrapper-about_us");
 about_us_header = about_us_wrapper.querySelector("header");
+
+const chronology_wrapper = document.getElementById("wrapper-chronology");
+chronology_header = chronology_wrapper.querySelector("header");
+
+const download_wrapper = document.getElementById("wrapper-download");
+download_header = download_wrapper.querySelector("header");
 
 function onDrag1({movementX, movementY})
 {
@@ -62,18 +68,68 @@ function onDrag1({movementX, movementY})
     about_us_wrapper.style.left = `${left + movementX}px` ;
     about_us_wrapper.style.top = `${top + movementY}px` ;
 }
+function onDrag2({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(chronology_wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    chronology_wrapper.style.left = `${left + movementX}px` ;
+    chronology_wrapper.style.top = `${top + movementY}px` ;
+}
+function onDrag3({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(download_wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    download_wrapper.style.left = `${left + movementX}px` ;
+    download_wrapper.style.top = `${top + movementY}px` ;
+}
 
 about_us_header.addEventListener("mousedown", () => {
   about_us_header.addEventListener("mousemove", onDrag1);
   about_us_wrapper.style.zIndex = "2";
+  chronology_wrapper.style.zIndex = "1";
+  download_wrapper.style.zIndex = "1";
 });
 
 about_us_header.addEventListener("mouseup", () => {
   about_us_header.removeEventListener("mousemove", onDrag1);
-  about_us_wrapper.style.zIndex = "1";
 });
 
 about_us_header.addEventListener("mouseleave", () => {
   about_us_header.removeEventListener("mousemove", onDrag1);
+});
+
+chronology_header.addEventListener("mousedown", () => {
+  chronology_header.addEventListener("mousemove", onDrag2);
   about_us_wrapper.style.zIndex = "1";
+  chronology_wrapper.style.zIndex = "2";
+  download_wrapper.style.zIndex = "1";
+});
+
+chronology_header.addEventListener("mouseup", () => {
+  chronology_header.removeEventListener("mousemove", onDrag2);
+});
+
+chronology_header.addEventListener("mouseleave", () => {
+  chronology_header.removeEventListener("mousemove", onDrag2);
+});
+
+download_header.addEventListener("mousedown", () => {
+  download_header.addEventListener("mousemove", onDrag3);
+  about_us_wrapper.style.zIndex = "1";
+  chronology_wrapper.style.zIndex = "1";
+  download_wrapper.style.zIndex = "2";
+});
+
+download_header.addEventListener("mouseup", () => {
+  download_header.removeEventListener("mousemove", onDrag3);
+});
+
+download_header.addEventListener("mouseleave", () => {
+  download_header.removeEventListener("mousemove", onDrag3);
 });
