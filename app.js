c4c613592a17c862a20d73688b3e3c2f7b6d063e
Jimmy-wil <103609594+Jimmy-wil@users.noreply.github.com>
2024-03-20 00:46:41 +0100

    ajout des fichier individuels

--- app.js
+++ app.js
@@ -1,87 +1,135 @@
-const reviews = [
-    {
-        id: 1,
-        name: "Jimmy",
-        job: "designer",
-        img: "img/pp.png",
-        text: "Jimmy, chargé de la direction artistique du jeu et de gloabelement tout l'aspect graphique de studion que ce soit du site en passant par la telecommuniatction",
-    },
-    {
-        id: 2,
-        name: "Gabriel",
-        job: "multijouer",
-        img: "styles/images/gear1.png", 
-        text: "Gabriel, chargé de faire le mutltiojour du jeu et joui je sais que jefais des rerreuds danqje vçuugvazoevguiyogvre je suis fatigué ok ???",
-    },
-    {
-        id: 3,
-        name: "Rania",
-        job: "interface",
-        img: "img/pp.png", 
-        text: "Rania, chargé de faire le mutltiojour du jeu et joui je sais que jefais des rerreuds danqje vçuugvazoevguiyogvre je suis fatigué ok ???",
-    },
-    {
-        id: 4,
-        name: "Nicolas",
-        job: "items",
-        img: "img/pp.png", 
-        text: "Niacolas, chargé de faire le mutltiojour du jeu et joui je sais que jefais des rerreuds danqje vçuugvazoevguiyogvre je suis fatigué ok ???",
-    },
-    {
-        id: 5,
-        name: "Chef de la vie et de la mort",
-        job: "IA",
-        img: "img/pp.png", 
-        text: "MA GROSSSE BITE AUSSI , chargé de faire le mutltiojour du jeu et joui je sais que jefais des rerreuds danqje vçuugvazoevguiyogvre je suis fatigué ok ???",
-    },
+// ------------- OPEN CLOSE DIV --------------
+const openAboutUsButton = document.getElementById("btn-about_us");
+const closeAboutUsButton = document.getElementById("btn-close-about_us");
 
-]
+const openChronologyButton = document.getElementById("btn-chronology");
+const closeChronologyButton = document.getElementById("btn-close-chronology");
 
-// select items
+const openDownloadButton = document.getElementById("btn-download");
+const closeDownloadButton = document.getElementById("btn-close-download");
 
-const img = document.getElementById("person-img");
-const author = document.getElementById("author");
-const job = document.getElementById("job");
-const info = document.getElementById("info");
+function openModal(modal){
+  if (modal == null) return
+  modal.classList.add("show")
+};
+function closeModal(modal){
+  if (modal == null) return
+  modal.classList.remove("show")
+};
 
-const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
 
-// set starting item
+openAboutUsButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-about_us")
+  openModal(window);
+});
+closeAboutUsButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-about_us")
+  closeModal(window);
+});
 
-let currentItem = 1;
+openChronologyButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-chronology")
+  openModal(window);
+});
+closeChronologyButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-chronology")
+  closeModal(window);
+});
 
-// load initial tem
+openDownloadButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-download")
+  openModal(window);
+});
+closeDownloadButton.addEventListener("click", () => {
+  const window = document.getElementById("wrapper-download")
+  closeModal(window);
+});
 
-window.addEventListener('DOMContentLoaded', function(){
-    const item = reviews[currentItem]
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-})
 
-function showPerson(person) {
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
-  }
-  // show next person
-  nextBtn.addEventListener('click', function () {
-    currentItem++;
-    if (currentItem > reviews.length - 1) {
-      currentItem = 0;
-    }
-    showPerson(currentItem);
-  });
-  // show prev person
-  prevBtn.addEventListener('click', function () {
-    currentItem--;
-    if (currentItem < 0) {
-      currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem);
-  });
-  
+// ----------------------------------
+
+// ------------- DRAGGABLE DIV --------------
+const about_us_wrapper = document.getElementById("wrapper-about_us");
+about_us_header = about_us_wrapper.querySelector("header");
+
+const chronology_wrapper = document.getElementById("wrapper-chronology");
+chronology_header = chronology_wrapper.querySelector("header");
+
+const download_wrapper = document.getElementById("wrapper-download");
+download_header = download_wrapper.querySelector("header");
+
+function onDrag1({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(about_us_wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    about_us_wrapper.style.left = `${left + movementX}px` ;
+    about_us_wrapper.style.top = `${top + movementY}px` ;
+}
+function onDrag2({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(chronology_wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    chronology_wrapper.style.left = `${left + movementX}px` ;
+    chronology_wrapper.style.top = `${top + movementY}px` ;
+}
+function onDrag3({movementX, movementY})
+{
+    let getStyle = window.getComputedStyle(download_wrapper);
+
+    let left = parseInt(getStyle.left); 
+    let top = parseInt(getStyle.top);
+
+    download_wrapper.style.left = `${left + movementX}px` ;
+    download_wrapper.style.top = `${top + movementY}px` ;
+}
+
+about_us_header.addEventListener("mousedown", () => {
+  about_us_header.addEventListener("mousemove", onDrag1);
+  about_us_wrapper.style.zIndex = "2";
+  chronology_wrapper.style.zIndex = "1";
+  download_wrapper.style.zIndex = "1";
+});
+
+about_us_header.addEventListener("mouseup", () => {
+  about_us_header.removeEventListener("mousemove", onDrag1);
+});
+
+about_us_header.addEventListener("mouseleave", () => {
+  about_us_header.removeEventListener("mousemove", onDrag1);
+});
+
+chronology_header.addEventListener("mousedown", () => {
+  chronology_header.addEventListener("mousemove", onDrag2);
+  about_us_wrapper.style.zIndex = "1";
+  chronology_wrapper.style.zIndex = "2";
+  download_wrapper.style.zIndex = "1";
+});
+
+chronology_header.addEventListener("mouseup", () => {
+  chronology_header.removeEventListener("mousemove", onDrag2);
+});
+
+chronology_header.addEventListener("mouseleave", () => {
+  chronology_header.removeEventListener("mousemove", onDrag2);
+});
+
+download_header.addEventListener("mousedown", () => {
+  download_header.addEventListener("mousemove", onDrag3);
+  about_us_wrapper.style.zIndex = "1";
+  chronology_wrapper.style.zIndex = "1";
+  download_wrapper.style.zIndex = "2";
+});
+
+download_header.addEventListener("mouseup", () => {
+  download_header.removeEventListener("mousemove", onDrag3);
+});
+
+download_header.addEventListener("mouseleave", () => {
+  download_header.removeEventListener("mousemove", onDrag3);
+});
